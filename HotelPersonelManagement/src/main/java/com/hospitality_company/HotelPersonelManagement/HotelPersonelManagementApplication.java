b2a91ef62541e0096c90e55af2156eae54caf45a
KrzysiekJa <kjmj77@gmail.com>
2022-05-05 02:01:25 +0200

    Added init backend

---
+++ HotelPersonelManagement/src/main/java/com/hospitality_company/HotelPersonelManagement/HotelPersonelManagementApplication.java
@@ -0,0 +1,14 @@
+package com.hospitality_company.HotelPersonelManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelPersonelManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HotelPersonelManagementApplication.class, args);
+	}
+
+}
---
+++ HotelPersonelManagement/src/test/java/com/hospitality_company/HotelPersonelManagement/HotelPersonelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitality_company.HotelPersonelManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelPersonelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
