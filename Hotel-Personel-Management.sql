0ed4f6f4ea510b7e703453416fb7a276f41cce1d
Piotr Fedirko <fedirko@student.agh.edu.pl>
2022-05-22 21:25:21 +0200

    added trigger

--- Hotel-Personel-Management.sql
+++ Hotel-Personel-Management.sql
@@ -1017,3 +1017,28 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+/*!50003 DROP TRIGGER IF EXISTS `vacation_days` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER //
+CREATE TRIGGER `vacation_days` AFTER INSERT ON `workplanemployees` FOR EACH ROW BEGIN
+	IF (SELECT `status` FROM `workplan` WHERE WorkPlan.shift_ID=NEW.shift_ID)="Vacation" THEN
+		SET @e_date=(SELECT ending_date FROM workplan WHERE workplan.shift_ID=NEW.shift_ID);
+        SET @s_date=(SELECT starting_date FROM workplan WHERE workplan.shift_ID=NEW.shift_ID);
+		UPDATE employees
+        SET employees.number_of_vacation_days=employees.number_of_vacation_days-DATEDIFF(@e_date, @s_date)
+        WHERE NEW.hotel_employee_ID=employees.employee_ID;
+	END IF;
+END;//
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
